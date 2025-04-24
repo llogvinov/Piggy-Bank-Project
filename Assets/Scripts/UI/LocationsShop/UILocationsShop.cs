01c6fc9c8da8ba4ffa2812b40090a74090a508b1
Lev Logvinov <llogvinov356@gmail.com>
2025-04-24 12:57:20 +0300

    Updated locations shop

--- Assets/Scripts/UI/LocationsShop/UILocationsShop.cs
+++ Assets/Scripts/UI/LocationsShop/UILocationsShop.cs
@@ -6,10 +6,6 @@ namespace UI.LocationsShop
 {
     public class UILocationsShop : MonoBehaviour, IItemShopUI
     {
-        [Header("Layout Settings")]
-        [SerializeField] private float itemSpacing = 5f;
-        [SerializeField] private float itemWidth;
-
         [Header("UI Elements")]
         [SerializeField] private Transform ShopItemsContainer;
         [SerializeField] private GameObject itemPrefab;
@@ -28,87 +24,53 @@ namespace UI.LocationsShop
         private void Start()
         {
             GenerateShopItemUI();
-
-            //Set selected location in the playerDataManager
             SetSelectedItem();
-
-            //Select UI item
             SelectItemUI(_playerDataService.GetSelectedLocationIndex());
-
-            //Update player skin
             ChangeItemSkin();
         }
 
-        //Generate UI Shop Item
         public void GenerateShopItemUI()
         {
-            //Loop through save purchased items and
-            //make them purchased in the Database array
             for (int i = 0; i < _playerDataService.GetAllPurchasedLocations().Count; i++)
             {
                 int purchaseLocationIndex = _playerDataService.GetPurchasedLocation(i);
                 locationDB.PurchaseLocation(purchaseLocationIndex);
             }
 
-            //Delete item template after calculating item's width
-            itemWidth = ShopItemsContainer.GetChild(0).GetComponent<RectTransform>().sizeDelta.x;
-            Destroy(ShopItemsContainer.GetChild(0).gameObject);
-            ShopItemsContainer.DetachChildren();
-
-            //Generate Items
             for (int i = 0; i < locationDB.LocationsCount; i++)
             {
                 Location location = locationDB.GetLocation(i);
                 LocationItemUI uiItem = Instantiate(itemPrefab, ShopItemsContainer).GetComponent<LocationItemUI>();
 
-                //Move item to its position
-                uiItem.SetItemPosition(Vector2.right * i * (itemWidth + itemSpacing));
-
-                //Set item name in Hierarchy
                 uiItem.gameObject.name = "Item" + i + "-" + location.name;
 
-                //Add information to the UI (one item)
                 uiItem.SetLocationName(location.name);
                 uiItem.SetLocationImages(location.sky, location.ground, location.trees, location.mountain);
                 uiItem.SetLocationPrice(location.price);
 
                 if (location.isPurchased)
                 {
-                    //Location is purchased
                     uiItem.SetItemAsPurchased();
                     uiItem.OnItemSelect(i, OnItemSelected);
                 }
                 else
                 {
-                    //Location is not purchased yet
                     uiItem.SetLocationPrice(location.price);
                     uiItem.OnItemPurchase(i, OnItemPurchased);
                 }
-
-                //ResizeItemsContainer
-                ShopItemsContainer.GetComponent<RectTransform>().sizeDelta =
-                    Vector2.right * (itemWidth + itemSpacing) * (locationDB.LocationsCount - 1);
             }
         }
 
         public void SetSelectedItem()
         {
-            //Get Saved index
             int index = _playerDataService.GetSelectedLocationIndex();
-
-            //Set selected index
             _playerDataService.SetSelectedLocation(locationDB.GetLocation(index), index);
         }
 
         public void OnItemSelected(int index)
         {
-            //Select item in the UI
             SelectItemUI(index);
-
-            //Save Data
             _playerDataService.SetSelectedLocation(locationDB.GetLocation(index), index);
-
-            //Change location
             ChangeItemSkin();
         }
 
@@ -138,19 +100,16 @@ namespace UI.LocationsShop
 
             if (_playerDataService.CanSpendCoins(location.price))
             {
-                //Proceed with the purchase operation
                 _playerDataService.SpendCoins(location.price);
                 GameSharedUI.Instance.UpdateCoinsUIText();
                 locationDB.PurchaseLocation(index);
                 locationUIItem.SetItemAsPurchased();
                 locationUIItem.OnItemSelect(index, OnItemSelected);
 
-                //Add purchased data on Shop Data
                 _playerDataService.AddPurchasedLocation(index);
             }
             else
             {
-                //Not enough coins
 #if UNITY_EDITOR
                 Debug.Log("Not Enough Coins!");
 #endif
